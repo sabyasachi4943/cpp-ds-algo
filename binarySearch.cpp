79b0c079307b0a4dcf65a65ba99e1e90cc2b8060
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-06-23 00:33:49 +0600

    2nd day

---
+++ binarySearch.cpp
@@ -0,0 +1,40 @@
+  #include<iostream>
+using namespace std;
+
+int binary_search(int arr[],int n, int key){
+  // implement binary search
+  int s = 0;
+  int e = n - 1;
+  
+  while(s<=e){
+    int mid = (s + e) / 2;
+
+    if(arr[mid] == key){
+      return mid;
+    }else if(arr[mid]> key){
+      e = mid - 1;
+    }else{
+      s = mid + 1;
+    }
+  }
+  return -1;
+}
+
+
+
+int main(){
+  int arr[] = {10, 20, 30, 40, 45, 50, 60, 70, 89};
+  int n = sizeof(arr) / sizeof(int);
+
+  int key;
+  cin >> key;
+
+  int index = binary_search(arr, n, key);
+  if(index!=-1){
+    cout << key << " is present at index " << index << endl;
+  }else{
+    cout << key << " is NOT found!" << endl;
+  }
+
+  return 0;
+}
