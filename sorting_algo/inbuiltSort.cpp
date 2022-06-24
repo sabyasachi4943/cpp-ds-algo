3cbbd37ee69aae222c927fc61b1865b62ac45a6d
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-06-24 23:18:00 +0600

    4th day

---
+++ sorting_algo/inbuiltSort.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+bool compare(int a, int b){
+  return a < b;
+}
+
+int main(){
+  int arr[] = {10, 9, 8, 6, 5, 4, 3, 2, 11, 16, 8};
+  int n = sizeof(arr) / sizeof(int);
+
+  sort(arr, arr + n, compare);
+  // reverse(arr, arr+n);
+  // print all the output
+  for(int x: arr){
+    cout << x << " ";
+  }
+
+  return 0;
+}
