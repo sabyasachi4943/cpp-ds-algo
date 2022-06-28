0780753971bca479ae5d35e2f16e83224d02029c
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-06-28 20:20:57 +0600

    7th day

---
+++ 3.character-ArraysStrings/string-class.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+
+int main(){
+  // char s[1000] ={'1','a','b','c','\0};
+  string s; //"hello world"; //dynamic array
+
+  int n = 5;
+  vector<string> sarr;
+  string temp;
+
+  while(n--){
+    getline(cin, temp);
+    sarr.push_back(temp);
+  }
+
+  for(string x: sarr){
+    cout << x << "," << endl;
+  }
+}
