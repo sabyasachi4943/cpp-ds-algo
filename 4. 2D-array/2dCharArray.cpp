0af36c0781bce0adc2d81e0fa5e2588d8f404c0d
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-06-30 21:45:26 +0600

    9th day

---
+++ 4. 2D-array/2dCharArray.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  char numbers[][10] = {
+      "one",
+      "two",
+      "three",
+      "forty",
+      "eight",
+      "hundred",
+  };
+
+  for (int i = 0; i < 6;i++){
+    cout << numbers[i] << endl;
+  }
+  return 0;
+}
